e8db1512e031f8a201bc7d4264a3382a9a641154
Ericsson Raphael <ericssonraphael@gmail.com>
2025-02-09 23:54:55 +0100

    Update: app.ts

--- app.ts
+++ app.ts
@@ -1,22 +1,21 @@
 import express, { Request, Response, NextFunction } from 'express';
 import bodyParser from 'body-parser';
-import setupSecurity from './src/middlewares/security';
-import itemRoutes from './src/routes/item.routes';
-import authRoutes from './src/routes/auth.routes';
+import setupSecurity from './middlewares/security';
+import itemRoutes from './routes/item.routes';
+import authRoutes from './routes/auth.routes';
 import passport from 'passport';
-import './src/config/passport';
-import dotenv from 'dotenv';
-import AppConfig from './src/config/app.config';
-import { AppError } from './src/utils/appError';
-import globalErrorHandler from './src/controllers/error.controller';
+import './config/passport';
+import AppConfig from './config/app.config';
+import { AppError } from './utils/appError';
+import globalErrorHandler from './controllers/error.controller';
 import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from './src/config/swagger.config';
-
-// Load environment variables
-dotenv.config();
+import swaggerSpec from './config/swagger.config';
 
 const app = express();
-const PORT = AppConfig.server.port || 3000;
+
+if (AppConfig.NODE_ENV == 'production') {
+  app.set('trust proxy', true);
+}
 
 // Middlewares
 app.use(bodyParser.json());
@@ -33,7 +32,7 @@ app.use('/api/items', itemRoutes);
 app.use('/api/auth', authRoutes);
 
 app.all('*', (req: Request, res: Response, next: NextFunction) => {
-    next(new AppError(`Can't find ${req.originalUrl} on this Server!`, 404));
+  next(new AppError(`Can't find ${req.originalUrl} on this Server!`, 404));
 });
 
 app.use(globalErrorHandler);
