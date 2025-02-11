a60c55e576f0548308455672f1918f3e86d6b7c2
Ericsson Raphael <ericssonraphael@gmail.com>
2025-02-11 21:02:51 +0100

    Fix: Swagger Production server alternative

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,21 +1,25 @@
 import swaggerJSDoc from 'swagger-jsdoc';
 
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Fresh-Track API',
-            version: '1.0.0',
-            description: 'API documentation for Fresh-Track',
-        },
-        servers: [
-            {
-                url: 'http://localhost:3000',
-                description: 'Development server',
-            },
-        ],
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Fresh-Track API',
+      version: '1.0.0',
+      description: 'API documentation for Fresh-Track',
     },
-    apis: ['./src/routes/*.ts', './src/models/*.ts'], // Adjust the paths as necessary
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server',
+      },
+      {
+        url: 'https://fresh-track-api.onrender.com',
+        description: 'Production server',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.ts', './src/models/*.ts'], // Adjust the paths as necessary
 };
 
 const swaggerSpec = swaggerJSDoc(options);
